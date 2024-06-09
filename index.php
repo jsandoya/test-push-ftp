dccc2e902b65f007694fef0c8d10af6856590fd0
José Sandoya <josesandoyaj@gmail.com>
2024-06-09 14:47:19 -0500

    subida

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 	<meta charset="utf-8"/>
 
-	<title>...Our website is coming soon...</title>
+	<title>..Our website is coming soon..</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"/>
 
